3e35f17b481ccb321246f25c4437687027770815
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-05-07 23:52:18 +0100

    chakra ui setup done

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+"use client";
+import { Button, Heading, Box } from '@chakra-ui/react'
+import React from 'react'
+
+const About = () => {
+  return (
+    <Box>
+      <Heading>About Component</Heading>
+      <Button colorScheme="blue">Button</Button>
+    </Box>
+  )
+}
+
+export default About
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
+"use client";
+import React from 'react'
+import { Providers } from './providers'
 import './globals.css'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
